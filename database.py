b3de2cb8c549c204c865a628dd924d6b11395f7c
ValeriiaEgorova <samszvd02@gmail.com>
2025-12-28 23:38:13 +0300

    modified:   database.py new file:   docker-compose.yml modified:   requirements.txt

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import os
-from dotenv import load_dotenv
 from sqlmodel import create_engine, SQLModel, Session
+from dotenv import load_dotenv
 
 load_dotenv()
 
-sqlite_url = os.getenv("DATABASE_URL", "sqlite:///database.db")
-engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
+database_url = os.getenv("DATABASE_URL")
+
+engine = create_engine(database_url)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
